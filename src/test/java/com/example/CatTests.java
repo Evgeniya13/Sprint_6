11c08ca420be82d725145a1f6dcf69bf39c82acd
Evgeniya Shirmanova <shirmanova13@gmail.com>
2024-05-20 16:49:39 +0200

    Sprint 6

--- src/test/java/com/example/CatTests.java
+++ src/test/java/com/example/CatTests.java
@@ -1,2 +1,30 @@
-package com.example;public class CatTests {
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class CatTests {
+
+    @Test
+    public void testGetSound() {
+        Cat cat = new Cat(new Feline());
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Cat cat = new Cat(new Feline());
+        List<String> food = cat.getFood();
+        Assert.assertEquals("Животные", food.get(0));
+        Assert.assertEquals("Птицы", food.get(1));
+        Assert.assertEquals("Рыба", food.get(2));
+    }
 }
